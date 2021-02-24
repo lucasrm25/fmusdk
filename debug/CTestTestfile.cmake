d06e4c3fbe2d365093a934e428cf5ca1d4710281
Lucas Rath <lucasrm25@gmail.com>
2021-02-24 15:48:45 +0100

    updating

--- debug/CTestTestfile.cmake
+++ debug/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test(test_springDamper_20_me "/home/lucas/Desktop/11_Github/FMI/fmusdk/dist/fmu20/me/fmusim_20_me" "/home/lucas/Desktop/11_Github/FMI/fmusdk/dist/fmu20/me/springDamper.fmu")
-set_tests_properties(test_springDamper_20_me PROPERTIES  ENVIRONMENT "FMUSDK_HOME=/home/lucas/Desktop/11_Github/FMI/fmusdk" WORKING_DIRECTORY "/home/lucas/Desktop/11_Github/FMI/fmusdk/temp/fmu20/me/springDamper" _BACKTRACE_TRIPLES "/home/lucas/Desktop/11_Github/FMI/fmusdk/CMakeLists.txt;289;add_test;/home/lucas/Desktop/11_Github/FMI/fmusdk/CMakeLists.txt;0;")
+set_tests_properties(test_springDamper_20_me PROPERTIES  ENVIRONMENT "FMUSDK_HOME=/home/lucas/Desktop/11_Github/FMI/fmusdk" WORKING_DIRECTORY "/home/lucas/Desktop/11_Github/FMI/fmusdk/temp/fmu20/me/springDamper" _BACKTRACE_TRIPLES "/home/lucas/Desktop/11_Github/FMI/fmusdk/CMakeLists.txt;292;add_test;/home/lucas/Desktop/11_Github/FMI/fmusdk/CMakeLists.txt;0;")
